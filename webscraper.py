035ff562d6ce2934499b4b672aa6218d0277e16a
Jesus Gil <jesusgil.dev@gmail.com>
2022-12-27 18:29:30 -0600

    add data txt files

--- webscraper.py
+++ webscraper.py
@@ -11,34 +11,34 @@ HEADERS = ({'User-Agent':
 ASIN = "B00005BIEF"
 
 url = f'https://www.amazon.com/dp/{ASIN}/'
-file_name = f"{ASIN}.txt"
+file_name = f"{ASIN}-2.txt"
 
-webpage = requests.get(url, headers=HEADERS)
-soup = BeautifulSoup(webpage.content, 'lxml')
-
-timestamp = datetime.datetime.now()
-
-# price
-current_price = soup.find('span', attrs={'class': 'a-offscreen'})
-current_price_string = current_price.string
-
-# ship from
-current_ship_from = soup.find('span', attrs={'class': 'tabular-buybox-text-message'})
-current_ship_from_string = current_ship_from.string
-
-# sold by
-current_sold_by = soup.find('a', attrs={'id': 'sellerProfileTriggerId'})
-current_sold_by_string = current_sold_by.string
 
 with open(file_name, "w") as file:
     file.write(f'Timestamp,ASIN,Price,Ship From,Sold By\n')
 
 def job():
 
+    webpage = requests.get(url, headers=HEADERS)
+    soup = BeautifulSoup(webpage.content, 'lxml')
+    timestamp = datetime.datetime.now()
+
+    # price
+    current_price = soup.find('span', attrs={'class': 'a-offscreen'})
+    current_price_string = current_price.string
+
+    # ship from
+    current_ship_from = soup.find('span', attrs={'class': 'tabular-buybox-text-message'})
+    current_ship_from_string = current_ship_from.string
+
+    # sold by
+    current_sold_by = soup.find('a', attrs={'id': 'sellerProfileTriggerId'})
+    current_sold_by_string = current_sold_by.string
+
     with open(file_name, "a") as file:
         file.write(f'{timestamp},{ASIN},{current_price_string},{current_ship_from_string},{current_sold_by_string}\n')
 
-schedule.every(5).seconds.until(datetime.timedelta(hours=24)).do(job)
+schedule.every(2).minutes.until(datetime.timedelta(hours=24)).do(job)
 while True:
     schedule.run_pending()
     time.sleep(1)
